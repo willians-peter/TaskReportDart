670bd6f20b05aa023c068b2d0a95f05284cb861d
Peter Cipriano <peterpanvoo@gmail.com>
2026-05-23 10:24:02 -0300

    feat: adicionado conteúdo padrão projeto

---
+++ tarefas_model.dart
@@ -0,0 +1,18 @@
+class Tarefa { 
+int id; 
+String titulo; 
+String responsavel; 
+String status; 
+String prioridade; 
+double valor; 
+int horas; 
+Tarefa({ 
+required this.id, 
+required this.titulo, 
+required this.responsavel, 
+required this.status, 
+required this.prioridade, 
+required this.valor, 
+required this.horas, 
+}); 
+} 
