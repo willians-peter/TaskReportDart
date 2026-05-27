d666822d1806aa668a81822b9717e314a2ec1a44
Peter Cipriano <peterpanvoo@gmail.com>
2026-05-27 20:16:18 -0300

    filtro por status

--- main.dart
+++ main.dart
@@ -24,4 +24,32 @@ void main() {
     print('Horas:  ${tarefa.horas}');
     imprimirLinha();
   }
+  //RF07 – Filtrar tarefas por status 
+  print('\n=== TAREFAS AGRUPADAS POR STATUS ===');
+  imprimirLinhaDupla();
+  final todosOsStatus = viewModel.tarefas.map((t) => t.status.toLowerCase().trim()).toSet();
+  for (var statusAtual in todosOsStatus) {
+    final tarefasDoStatus = viewModel.tarefas
+        .where((t) => t.status.toLowerCase().trim() == statusAtual)
+        .toList();
+    String nomeGrupo = statusAtual[0].toUpperCase() + statusAtual.substring(1);
+    print('\n STATUS: $nomeGrupo (${tarefasDoStatus.length} tarefa(s))');
+    imprimirLinha();
+    for (var tarefa in tarefasDoStatus) {
+      print('   [ID ${tarefa.id}] ${tarefa.titulo}');
+      print('    Responsável: ${tarefa.responsavel} | Prioridade: ${tarefa.prioridade}');
+      print('    Tempo: ${tarefa.horas}h | Custo: R\$ ${tarefa.valor}');
+      imprimirLinha();
+    }
+  }
+    imprimirLinhaDupla();
 }
+  //RF08 – Somar valores das tarefas concluídas 
+  //RF09 – Calcular média de valor das tarefas pendentes 
+  //RF10 – Calcular total de horas por status 
+  //RF11 – Identificar tarefas com dados incompletos 
+  //RF12 – Exibir status únicos usando Set 
+  //RF13 – Criar classe base e classe filha 
+  //RF14 – Aplicar encapsulamento 
+  //RF15 – Gerar relatório final 
+
